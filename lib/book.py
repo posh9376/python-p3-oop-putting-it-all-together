cecbaf6e58042bc67a602f4d051a96605b381b44
Eric Richard <reric0802@gmail.com>
2024-12-10 09:01:17 +0300

    added code in shoe and book class

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        if not isinstance(page_count, int):
+            raise TypeError("page_count must be an integer")
+        self._page_count = page_count
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        self._page_count = value
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand ,size):
+        self._brand = brand
+        self._size = size
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+    
+    @property
+    def brand (self):
+        return self._brand
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,value):
+        if not isinstance(value, int):
+            print('size must be an integer')
+        self._size = value
+    @brand.setter
+    def brand(self,value):
+        if not isinstance(value,str):
+            print('brand must be a string')
+        self._brand = value
+    
+    @property
+    def condition(self):
+        return self._condition
